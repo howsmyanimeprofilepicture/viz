085b65a831015f730f096b41d6e4a9f21369b4e3
howsmyanimeprofilepicture <howsmyanimeprofilepicture@gmail.com>
2022-03-11 20:02:02 +0900

    first

---
+++ saturn.py
@@ -0,0 +1,123 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation, FFMpegWriter
+import matplotlib.animation as animation
+from matplotlib.patches import Ellipse
+
+
+class Main:
+    def __init__(
+        self,
+        num_of_node: int = 100,
+        basis_of_width=0.4,
+        basis_of_height=0.4,
+        basic_size: float = 0.25,
+        w_weight: float = -0.065,
+        x_lim=5,
+        y_lim=1.5,
+        smooth=0.1,
+    ):
+        self.fig, self.ax = plt.subplots(figsize=(10, 10))
+        self.ax.set_aspect("equal")
+        self.ax.set_frame_on(False)
+        self.ax.set_xticks([])
+        self.ax.set_yticks([])
+        self.ax.set_xlim([-x_lim - 1, x_lim + 1])
+        self.ax.set_ylim([-y_lim - 1, y_lim + 1])
+
+        (
+            self.num_of_node,
+            self.basis_of_width,
+            self.basis_of_height,
+            self.basic_size,
+            self.w_weight,
+            self.x_lim,
+            self.y_lim,
+            self.smooth,
+        ) = (
+            num_of_node,
+            basis_of_width,
+            basis_of_height,
+            basic_size,
+            w_weight,
+            x_lim,
+            y_lim,
+            smooth,
+        )
+
+        X = np.random.uniform(
+            -self.x_lim, self.x_lim, size=(self.num_of_node, 1)
+        )  # (100 X 1)
+        Y = np.random.uniform(
+            -self.y_lim, self.y_lim, size=(self.num_of_node, 1)
+        )  # (100 X 1)
+
+        self.XY = np.concatenate([X, Y], axis=1)  # (100 X 2)
+
+        self.next_XY = self.XY
+        self.artists = []
+
+        self.color_and_alpha = np.random.rand(
+            self.num_of_node, 4
+        )  # (100 X 4) R, G, B, alpha
+        self.next_color_and_alpha = self.color_and_alpha
+
+    def fill_next_XY(self):
+        n = self.num_of_node - len(self.next_XY)
+        X = np.random.uniform(
+            -self.x_lim, -self.x_lim + self.smooth, size=(n, 1)
+        )  # (n X 1)
+        Y = np.random.uniform(-self.y_lim, self.y_lim, size=(n, 1))  # (n X 1)
+        XY = np.concatenate([X, Y], axis=1)  # (n X 2)
+
+        new_color_and_alpha = np.random.rand(n, 4)  # (n X 4)
+
+        self.next_XY = np.concatenate([self.next_XY, XY], axis=0)  # (100 X 2)
+        self.next_color_and_alpha = np.concatenate(
+            [self.next_color_and_alpha, new_color_and_alpha], axis=0,
+        )  # (100 X 2)
+
+    def __call__(self):
+        def update(i):
+            # Baton touch
+            self.XY = self.next_XY
+            self.color_and_alpha = self.next_color_and_alpha
+            self.next_XY = []
+            self.next_color_and_alpha = []
+            self.artists = []
+
+            for i, (x, y) in enumerate(self.XY):
+                if x < 5 - self.smooth:
+                    self.next_XY.append([x + self.smooth, y])
+                    self.next_color_and_alpha.append(self.color_and_alpha[i, :])
+
+                weight_coeff: float = np.exp(self.w_weight * (x ** 2))
+
+                ellipse = Ellipse(
+                    xy=[x, weight_coeff * y],
+                    width=weight_coeff * self.basis_of_width * np.random.rand(),
+                    height=weight_coeff * self.basis_of_height * np.random.rand(),
+                    facecolor=self.color_and_alpha[i, 0:3],
+                    alpha=(weight_coeff * self.color_and_alpha[i, 3]),
+                    angle=np.random.rand() * 90,
+                )
+                arti = self.ax.add_artist(ellipse)
+                self.artists.append(arti)
+
+            self.next_XY = np.array(self.next_XY)
+            self.next_color_and_alpha = np.array(self.next_color_and_alpha)
+            self.fill_next_XY()
+
+            return self.artists
+
+        ani = FuncAnimation(self.fig, update, interval=40, blit=True, save_count=1000)
+
+        # Writer = animation.writers["ffmpeg"]
+        # writer = Writer(fps=20, metadata=dict(artist="Me"), bitrate=1800)
+        # ani.save("g.mp4", writer=writer)
+        plt.show()
+
+
+if __name__ == "__main__":
+    main = Main()
+    main()
